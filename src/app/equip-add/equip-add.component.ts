1b4b55e049752ba1d7634c9bfcded3a930284cf2
Ahmed Selim <sci.ahmedselim@gmail.com>
2021-10-05 14:26:36 +0200

    add-history page updated!

--- src/app/equip-add/equip-add.component.ts
+++ src/app/equip-add/equip-add.component.ts
@@ -13,23 +13,34 @@ export class EquipAddComponent implements OnInit {
   
   @Output() newStatusEvent = new EventEmitter<string>();
 
-  history!: History;
-
-  equipments!: Observable<{ id: number; title: string; }[]>;
-
+  Equipments!: Observable<{ id: number; title: string; }[]>;
+  Parts: RepairParts[] = [
+    {id: 1 ,title: 'part1' ,sec_id: 1 ,equip_id: 1 ,quantity: 1 },
+    {id: 2 ,title: 'part2' ,sec_id: 1 ,equip_id: 1 ,quantity: 1 },
+  ] ;
+  Bugs: Bug[] = [
+    {id: 1 ,title: 'bug1' ,sec_id: 1 ,equip_id: 1 ,quantity: 1 },
+    {id: 2 ,title: 'bug2' ,sec_id: 1 ,equip_id: 1 ,quantity: 1 },
+  ] ;
+  
+  parts = this.Parts ;
+  bugs = this.Bugs ;
+  selectedBugs : Bug[] = [] ;
+  selectedParts : RepairParts[] = [] ;
+  
   equip_id!: number;
   equip_serial!: number;
   register_date!: string;
   report!: string;
-
+  
   result!: string ;
-
+  history!: History;
+  
   constructor(private authService: AuthService, private userService: UserService) { }
 
   ngOnInit(): void {
-    this.equipments = this.userService.section_equipments ;
-    this.equipments.subscribe((result: any) => console.log(result)) ;
-    
+    this.Equipments = this.userService.section_equipments ;
+    // this.Equipments.subscribe((result: any) => console.log(result)) ;
   }
 
   addHistory () {
@@ -56,4 +67,61 @@ export class EquipAddComponent implements OnInit {
     (e) => this.newStatusEvent.emit("error")) ;
   }
 
+  add_part(index: string) {
+    if (!index) {
+      return ;
+    }
+    let i = Number(index) ;
+    this.selectedParts.push(this.parts[i]) ;
+    this.parts.splice(i,1) ;
+  }
+
+  remove_selected_part(index: number) {
+    let part = this.selectedParts[index] ;
+    part.quantity = 1 ;
+    this.selectedParts.splice(index,1) ;
+    this.parts.push(part) ;
+  }
+  
+  add_bug(index: string) {
+    if (!index) {
+      return ;
+    }
+    let i = Number(index) ;
+    this.selectedBugs.push(this.bugs[i]) ;
+    this.bugs.splice(i,1) ;
+  }
+
+  remove_selected_bug(index: number) {
+    let bug = this.selectedBugs[index] ;
+    bug.quantity = 1 ;
+    this.selectedBugs.splice(index,1) ;
+    this.bugs.push(bug) ;
+  }
+
+  print() {
+    console.log("Equipment id : " , this.equip_id);
+    console.log("Equipment serial : " , this.equip_serial);
+    console.log("Register date : " , this.register_date);
+    console.log("Notes : " , this.report);
+
+    console.log(" Bugs : " , this.selectedBugs);
+    console.log(" Parts : " , this.selectedParts);    
+  }
+}
+
+interface RepairParts {
+  id: number ;
+  title: string ;
+  sec_id: number ;
+  equip_id: number ;
+  quantity: number ;
+}
+
+interface Bug {
+  id: number ;
+  title: string ;
+  sec_id: number ;
+  equip_id: number ;
+  quantity: number ;
 }
